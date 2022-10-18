3755d19a388dd60e9480db873d34871f0b096c5e
Jun Zhou <zhoujun97099@gmail.com>
2022-10-18 13:44:43 +0200

    implemented ProgramRunner class

--- Actividades/src/org/uem/dam/GestorProg/ProgramRunner.java
+++ Actividades/src/org/uem/dam/GestorProg/ProgramRunner.java
@@ -4,8 +4,24 @@ package org.uem.dam.GestorProg;
 This class runs the selected program
  */
 
+import javax.print.attribute.standard.PresentationDirection;
+import java.io.IOException;
+
 public class ProgramRunner {
     public void ProgramRunner(String programName) {
-        // TODO Auto-generated constructor stub
+        try {
+
+            ProcessBuilder pb = new ProcessBuilder(programName);
+            pb.redirectInput(ProcessBuilder.Redirect.INHERIT);
+            pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+            pb.redirectError(ProcessBuilder.Redirect.INHERIT);
+            Process process = pb.start();
+
+        } catch (IOException e) {
+            System.err.println("Excepción de E/S");
+        } catch (Exception e) {
+            System.err.println("Se ha producido un error");
+        }
+
     }
 }
